58838c91ffdb642ad46f1997304f6e879a6d34c7
zhaowenjie <zhaowenjie@megvii.com>
2022-06-30 17:52:55 +0800

    setup github action

---
+++ hack/install-bazel.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Cause the script to exit if a single command fails
+set -e
+
+platform="unknown"
+unamestr="$(uname)"
+archstr="$(uname -m)"
+arch="x86_64"
+if [[ "$unamestr" == "Linux" ]]; then
+  platform="linux"
+elif [[ "$unamestr" == "Darwin" ]]; then
+  platform="darwin"
+else
+  echo "Unrecognized platform."
+  exit 1
+fi
+
+if [[ "$archstr" == "arm64" ]] || [[ "$archstr" == "aarch64" ]]; then
+  arch="arm64"
+fi
+
+VERSION=4.2.1
+
+echo "Platform is ${platform}-${arch}, will install bazel $VERSION"
+
+if test $# -gt 0; then
+    VERSION=$1
+fi
+
+curl -L -o /tmp/install.sh "https://github.com/bazelbuild/bazel/releases/download/${VERSION}/bazel-${VERSION}-installer-${platform}-${arch}.sh"
+chmod +x /tmp/install.sh
+/tmp/install.sh --user
+rm -f /tmp/install.sh
